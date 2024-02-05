9762909c8dd679ca7778c171ed82bdc811f5446f
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-02-05 18:02:52 +0530

    new

--- src/components/ClickStart.jsx
+++ src/components/ClickStart.jsx
@@ -6,10 +6,10 @@ function ClickStart() {
         <div className=" mx-auto  justify-center mt-5 mb-2  flex">
             <a
               href="https://test.kix.digital/?utm_source=landingpage&utm_medium=website&utm_campaign=tradeathon&utm_content=wave3&utm_term=1millionkixtokens" 
-              className=" uppercase archo-black text-4xl sm:text-5xl txt-s2 py-2 px-4 sm:px-6   bg-[#fbe000] border-2 border-[#94339d]   mx-auto btn-hov rounded-xl  text-[#94339d]"
+              className="  tracking-[-0.11em] uppercase archo-black text-4xl sm:text-7xl  py-2 px-4 sm:px-6   bg-[#fbe000] border-2 border-[#94339d]   mx-auto btn-hov rounded-xl  text-[#94339d]"
             >
               {" "}
-              Play <span className="text-[#000]">Now!</span>
+              Play <span>Now</span>
               {/* <img src="images/click.png" alt="" className=" h-[34px] sm:h-[45px]" /> */}
             </a>
           </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,37 +18,37 @@ function Hero() {
         <img
           src="images/chart.png"
           alt=""
-          className=" absolute -z-[2] top-[45%] sm:top-[20%]"
+          className=" absolute -z-[2] top-[45%] sm:top-[16%]"
         />
-        <div className="py-3 sm:py-2">
+        <div className="py-3 sm:py-2 flex items-center gap-4">
           <img
             src="images/logo.png"
             alt=""
             className=" mx-auto h-[2rem] sm:h-[40px]"
-          />
+          /> <h2 className=" font-work text-4xl font-normal text-white">TRADE-ATHON </h2>
         </div>
         <div className=" sm:pt-0 pt-[40%]">
           <img
             src="images/tredtext.png"
             alt=""
-            className=" mx-auto py-1 -mt-3  sm:-mt-0 sm:py-4 sm:h-[6.5rem] h-[3.5rem] "
+            className=" mx-auto py-1 -mt-3  sm:-mt-6 sm:py-4 sm:h-[9.5rem] h-[3.5rem] "
           />
         </div>
 
-        <div className=" relative ">
+        {/* <div className=" relative ">
           <h1
             className="  text-white  lh-1_1 
           sm:text-2xl text-lg  text-center "
           >
             BUY & SELL FOOTBALLERS
           </h1>
-        </div>
+        </div> */}
 
         <div>
           <img
             src="/teen.png"
             alt=""
-            className=" mx-auto h-auto sm:h-[300px]"
+            className=" mx-auto h-auto sm:h-[250px]"
           />
         </div>
 
@@ -67,6 +67,7 @@ function Hero() {
             alt=""
             className=" mx-auto h-auto sm:h-[105px]"
           />
+          <p className=" font-work text-4xl text-white "> <span className=" archo-black">+</span> 1,000,000 $KIX</p>
         </div>
       </div>
     </section>
